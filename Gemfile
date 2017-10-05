d7a4969f7515c8a1133e71e46b0bf7a5e6b9b811
Andrew Cates <amcates@gmail.com>
2017-10-05 09:36:09 -0400

    adding ruby racer and fixing deploy directory for testing

--- Gemfile
+++ Gemfile
@@ -39,3 +39,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 gem 'mysql2'
+
+gem 'therubyracer'
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,6 @@
-server 'bmic-sparc-dev.obis.musc.edu/', user: 'capistrano', roles: %w{app db web}
+server 'bmic-sparc-dev.obis.musc.edu', user: 'capistrano', roles: %w{app db web}
 
-set :deploy_to, '/var/www/rails/biobank'
+set :deploy_to, '/var/www/rails/sparc-biobank'
 set :branch, 'testing'
 set :rails_env, 'production'
 set :rvm_ruby_version, '2.4.1@biobank --create'
