8b5ac20dce93619aa6c923d7539c4e700df025ec
Andrew Cates <amcates@gmail.com>
2017-10-05 09:39:03 -0400

    symlink .env

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,8 @@ set :linked_files, fetch(:linked_files, []).push('config/database.yml',
                                                  'config/secrets.yml',
                                                  'config/shards.yml',
                                                  '.ruby-version',
-                                                 '.ruby-gemset'
+                                                 '.ruby-gemset',
+                                                 '.env'
                                                 )
 set :linked_dirs, fetch(:linked_dirs, []).push('log',
                                                'tmp/pids',
