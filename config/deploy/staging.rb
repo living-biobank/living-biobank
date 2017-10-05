1900c5ab83185b3940e6c6fa0dc4bb646d6519de
Andrew Cates <amcates@gmail.com>
2017-10-05 09:41:46 -0400

    staging is using testing env, same as all other setups on this machine

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,7 +2,7 @@ server 'bmic-sparc-dev.obis.musc.edu', user: 'capistrano', roles: %w{app db web}
 
 set :deploy_to, '/var/www/rails/sparc-biobank'
 set :branch, 'testing'
-set :rails_env, 'production'
+set :rails_env, 'testing'
 set :rvm_ruby_version, '2.4.1@biobank --create'
 set :passenger_restart_with_touch, true
 
