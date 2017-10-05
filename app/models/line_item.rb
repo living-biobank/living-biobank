3d61558591ec56e13e769181fed0c44c814deaff
Andrew Cates <amcates@gmail.com>
2017-10-05 11:45:32 -0400

    working on fixes for new sparc structure

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,7 +1,6 @@
 class LineItem < ApplicationRecord
   include SparcShard
   belongs_to :sub_service_request
-  has_one :submission
 
   def self.submitted_line_items(service_id)
     joins(:sub_service_request)
@@ -9,11 +8,11 @@ class LineItem < ApplicationRecord
       .where(service_id: service_id)
   end
 
-  def i2b2_query
-    submission.questionnaire_responses.where(item_id: ENV.fetch('ITEM_ID')).first.content
+  def submission
+
   end
 
-  def protocol_id
-    submission.protocol_id
+  def i2b2_query
+    submission.questionnaire_responses.where(item_id: ENV.fetch('ITEM_ID')).first.content
   end
 end
