2b6b706d53ceb8248faf50255eb4349660951bea
Andrew Cates <amcates@gmail.com>
2017-10-16 11:32:48 -0400

    updating login view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171012130321) do
+ActiveRecord::Schema.define(version: 20171016135051) do
 
   create_table "labs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.bigint "patient_id"
@@ -86,6 +86,7 @@ ActiveRecord::Schema.define(version: 20171012130321) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "email", default: "", null: false
+    t.string "net_id"
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
