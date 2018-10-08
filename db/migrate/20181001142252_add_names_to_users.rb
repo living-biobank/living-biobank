824e2a7fd80af5ce144bf355475e950ce9089005
Kyle Glick <kyleglick99@gmail.com>
2018-10-08 11:05:48 -0500

    fix user name migration

--- db/migrate/20181001142252_add_names_to_users.rb
+++ db/migrate/20181001142252_add_names_to_users.rb
@@ -5,7 +5,7 @@ class AddNamesToUsers < ActiveRecord::Migration[5.1]
   end
 
   def down
-    drop_column :users, :first_name
-    drop_column :users, :last_name
+    remove_column :users, :first_name
+    remove_column :users, :last_name
   end
 end
