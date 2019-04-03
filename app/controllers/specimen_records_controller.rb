57d1c22b32b74b0cac5fdc19537f42a7fdd6514f
Kyle Glick <kyleglick99@gmail.com>
2019-04-03 15:50:55 -0500

    comment out cloverleaf messenger

--- app/controllers/specimen_records_controller.rb
+++ app/controllers/specimen_records_controller.rb
@@ -25,7 +25,7 @@ class SpecimenRecordsController < ApplicationController
     if @specimen_record.save
       @lab_groups = Lab.available.includes(patient: :specimen_requests).group_by{ |l| { patient: l.patient, specimen_source: l.specimen_source } }
 
-      Delayed::Job.enqueue CloverleafMessengerJob.new(@patient.labs.available.first)
+      # Delayed::Job.enqueue CloverleafMessengerJob.new(@patient.labs.available.first)
 
       flash.now[:success] = t(:specimen_records)[:created]
     else
