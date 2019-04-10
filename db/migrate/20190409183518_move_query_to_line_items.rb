4f469c5c34dd4332d94401951cb7e7852a6fc423
Kyle Glick <kyleglick99@gmail.com>
2019-04-10 09:11:07 -0500

    fix migration

--- db/migrate/20190409183518_move_query_to_line_items.rb
+++ db/migrate/20190409183518_move_query_to_line_items.rb
@@ -2,9 +2,6 @@ class MoveQueryToLineItems < ActiveRecord::Migration[5.1]
   def up
     sparc_requests = SparcRequest.all
 
-    remove_column :sparc_requests, :query_name
-    remove_column :sparc_requests, :query_count
-
     add_column :line_items, :query_name, :string, after: :service_source
     add_column :line_items, :query_count, :integer, after: :query_name
 
@@ -13,5 +10,8 @@ class MoveQueryToLineItems < ActiveRecord::Migration[5.1]
     sparc_requests.each do |request|
       request.line_items.update_all(query_name: request.query_name, query_count: request.query_count)
     end
+
+    remove_column :sparc_requests, :query_name
+    remove_column :sparc_requests, :query_count
   end
 end
