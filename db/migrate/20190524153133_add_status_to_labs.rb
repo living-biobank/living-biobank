6bcdd7ec052ceec5ec1ace7b08774e649c89f797
Kyle Glick <kyleglick99@gmail.com>
2019-06-05 14:33:27 -0500

    fix migration

--- db/migrate/20190524153133_add_status_to_labs.rb
+++ db/migrate/20190524153133_add_status_to_labs.rb
@@ -1,5 +1,5 @@
 class AddStatusToLabs < ActiveRecord::Migration[5.1]
   def change
-  	add_column :labs, :status, :string, default: t(:labs)[:statuses][:available]
+  	add_column :labs, :status, :string, default: I18n.t(:labs)[:statuses][:available]
   end
 end
