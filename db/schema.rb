90217bb39615eaa505ae9da005851c588d963293
Ito-Eta <ito@Andrews-MacBook-Pro.local>
2019-06-06 13:13:04 -0400

    another push

--- db/schema.rb
+++ db/schema.rb
@@ -71,7 +71,7 @@ ActiveRecord::Schema.define(version: 20190531185326) do
     t.string "bio_bank_pref"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "identifer"
+    t.string "identifier"
     t.date "dob"
   end
 
