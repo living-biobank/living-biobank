c8caf5bce697854690410fa15e43f91047ca6a43
Andrew Cates <amcates@gmail.com>
2019-07-02 12:52:00 -0400

    reverting deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,10 +3,18 @@ lock "3.11.0"
 set :application, "biobank"
 set :repo_url, "git@sparc_biobank:HSSC/sparc-biobank.git"
 
-append :linked_files,
-  'config/database.yml', 'config/secrets.yml', 'config/ldap.yml',
-  'config/i2b2_database.yml', 'config/sparc_request_database.yml',
-  '.ruby-version', '.ruby-gemset', '.env'
-
-append :linked_dirs,
-  'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'bundle', 'node_modules'
+set :linked_files, fetch(:linked_files, []).push('config/database.yml',
+                                                 'config/secrets.yml',
+                                                 'config/i2b2_database.yml',
+                                                 'config/sparc_request_database.yml',
+                                                 'config/ldap.yml',
+                                                 '.ruby-version',
+                                                 '.ruby-gemset',
+                                                 '.env'
+                                                )
+set :linked_dirs, fetch(:linked_dirs, []).push('log',
+                                               'tmp/pids',
+                                               'tmp/cache',
+                                               'tmp/sockets',
+                                               'public/system'
+                                              )
