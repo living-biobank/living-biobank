809a417753cf2ea8980e57eac8b2605401a9ca21
Ito-Eta <ito@Andrews-MacBook-Pro.local>
2019-07-10 10:33:39 -0400

    fixing minor errors

--- db/migrate/20190708144647_create_multiple_honest_broker_types.rb
+++ db/migrate/20190708144647_create_multiple_honest_broker_types.rb
@@ -1,5 +1,5 @@
 class CreateMultipleHonestBrokerTypes < ActiveRecord::Migration[5.1]
   def change
-    change_column :users, :honest_broker, :bigint, default: 0
+    change_column :users, :honest_broker, :integer, default: 0
   end
 end
