72083015b4c54a6770eb56ff23c30b7673ef5eb8
kayla-glick <kaylaglick12@gmail.com>
2020-03-31 17:28:55 -0400

    update deploy ruby versions

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@ server 'lbb.musc.edu', user: 'capistrano', roles: %w{app db web}
 set :deploy_to, '/var/www/rails/sparc-biobank'
 set :branch, 'production'
 set :rails_env, 'production'
-set :rvm_ruby_version, '2.4.2@sparc-biobank --create'
+set :rvm_ruby_version, '2.5.5@sparc-biobank --create'
 set :passenger_restart_with_touch, true
 
 set :default_env, {
--- config/deploy/testing.rb
+++ config/deploy/testing.rb
@@ -3,7 +3,7 @@ server 'bmic-sparc-dev.obis.musc.edu', user: 'capistrano', roles: %w{app db web}
 set :deploy_to, '/var/www/rails/sparc-biobank'
 set :branch, 'testing'
 set :rails_env, 'testing'
-set :rvm_ruby_version, '2.4.2@sparc-biobank --create'
+set :rvm_ruby_version, '2.5.5@sparc-biobank --create'
 set :passenger_restart_with_touch, true
 
 set :default_env, {
