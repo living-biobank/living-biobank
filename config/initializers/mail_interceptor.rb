71bbc6a9bb717b1f94613ddae926445251bc45eb
kayla-glick <kaylaglick12@gmail.com>
2020-04-10 16:36:16 -0400

    reroute non-production emails

--- config/initializers/mail_interceptor.rb
+++ config/initializers/mail_interceptor.rb
@@ -1,11 +1,16 @@
-class AddAppNameToEmailSubject
+class MailInterceptor
   def self.delivering_email(mail)
     prefixes  = []
     prefixes << I18n.t('site_information.name')
     prefixes << Rails.env.upcase unless Rails.env.production?
     prefix    = "[#{prefixes.join(' ')}] "
     mail.subject.prepend(prefix)
+
+    unless Rails.env.production?
+      mail.to = ENV.fetch('REPLY_EMAIL')
+      mail.cc = nil
+    end
   end
 end
 
-ActionMailer::Base.register_interceptor(AddAppNameToEmailSubject)
+ActionMailer::Base.register_interceptor(MailInterceptor)
