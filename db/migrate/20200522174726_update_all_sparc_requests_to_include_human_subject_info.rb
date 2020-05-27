fa6e4a99137776a42a451fd8fed6841ae07cbab5
kayla-glick <kaylaglick12@gmail.com>
2020-05-27 09:42:38 -0400

    fix migration for -d permissions issue

--- db/migrate/20200522174726_update_all_sparc_requests_to_include_human_subject_info.rb
+++ db/migrate/20200522174726_update_all_sparc_requests_to_include_human_subject_info.rb
@@ -1,6 +1,6 @@
 class UpdateAllSparcRequestsToIncludeHumanSubjectInfo < ActiveRecord::Migration[5.2]
   def change
-    SparcRequest.joins(:protocol).each do |sr|
+    SparcRequest.where(protocol_id: SPARC::Protocol.all.ids).each do |sr|
       if sr.protocol.research_types_info.present?
         sr.protocol.research_types_info.update(human_subjects: true)
       else
