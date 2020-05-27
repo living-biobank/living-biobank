4cb9c877cf8b8c45b667072240ea25957c9e0a3f
kayla-glick <kaylaglick12@gmail.com>
2020-05-27 09:35:45 -0400

    fix migration

--- db/migrate/20200522174726_update_all_sparc_requests_to_include_human_subject_info.rb
+++ db/migrate/20200522174726_update_all_sparc_requests_to_include_human_subject_info.rb
@@ -1,7 +1,7 @@
 class UpdateAllSparcRequestsToIncludeHumanSubjectInfo < ActiveRecord::Migration[5.2]
   def change
-    SparcRequest.all.each do |sr|
-      if sr.protocol.create_research_types_info.present?
+    SparcRequest.joins(:protocol).each do |sr|
+      if sr.protocol.research_types_info.present?
         sr.protocol.research_types_info.update(human_subjects: true)
       else
         sr.protocol.create_research_types_info(human_subjects: true)
