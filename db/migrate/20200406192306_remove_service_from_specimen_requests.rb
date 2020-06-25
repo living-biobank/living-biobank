47682d937e2483bd4a5603d1c15eed2b20ad2b54
kayla-glick <kaylaglick12@gmail.com>
2020-06-25 12:53:21 -0400

    fix migration

--- db/migrate/20200406192306_remove_service_from_specimen_requests.rb
+++ db/migrate/20200406192306_remove_service_from_specimen_requests.rb
@@ -3,6 +3,10 @@ class RemoveServiceFromSpecimenRequests < ActiveRecord::Migration[5.2]
     default_scope { order(:id) }
   end
 
+  class Variable < ApplicationRecord
+    default_scope { order(:id) }
+  end
+
   def change
     LineItem.specimen_requests.update_all(service_id: nil)
     lis = LineItem.additional_services.where.not(service_id: Service.all.pluck(:sparc_id) + Variable.all.pluck(:service_id))
