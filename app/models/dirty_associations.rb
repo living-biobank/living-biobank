7ec2b013969509c046d678a8c7925e296ccf945e
kayla-glick <kaylaglick12@gmail.com>
2020-07-15 12:17:52 -0400

    combining services+variables, updating statuses and request services logic

--- app/models/dirty_associations.rb
+++ app/models/dirty_associations.rb
@@ -39,7 +39,6 @@ module DirtyAssociations
     @changes_hash["#{record.class.name.underscore}".to_sym].merge!(current_hash)
 
     self.define_singleton_method("#{record.class.name.underscore}_records_changed".to_sym) do 
-      puts @changes_hash
       return @changes_hash
     end
   end
