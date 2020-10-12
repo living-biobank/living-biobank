369f330327427095754d100accd803b904279b7b
kayla-glick <kaylaglick12@gmail.com>
2020-10-12 14:59:02 -0400

    move testing to v1.1.1

--- config/deploy/testing.rb
+++ config/deploy/testing.rb
@@ -1,7 +1,7 @@
 server 'bmic-sparc-dev.obis.musc.edu', user: 'capistrano', roles: %w{app db web}
 
 set :deploy_to, '/var/www/rails/sparc-biobank'
-set :branch, 'testing'
+set :branch, 'v1.1.1'
 set :rails_env, 'testing'
 set :rvm_ruby_version, '2.5.5@sparc-biobank --create'
 set :passenger_restart_with_touch, true
