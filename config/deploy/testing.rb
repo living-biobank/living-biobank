acb6c7dd964be05c3db441ee621bd741f25d248c
kayla-glick <kaylaglick12@gmail.com>
2020-10-19 14:27:11 -0400

    update testing branch

--- config/deploy/testing.rb
+++ config/deploy/testing.rb
@@ -1,7 +1,7 @@
 server 'bmic-sparc-dev.obis.musc.edu', user: 'capistrano', roles: %w{app db web}
 
 set :deploy_to, '/var/www/rails/sparc-biobank'
-set :branch, 'v1.1.1'
+set :branch, 'testing'
 set :rails_env, 'testing'
 set :rvm_ruby_version, '2.5.5@sparc-biobank --create'
 set :passenger_restart_with_touch, true
