d43c6d6ba4932bdf9230ce7795a8211998e5db02
kayla-glick <kaylaglick12@gmail.com>
2020-11-06 13:14:39 -0500

    fixing migration

--- db/migrate/20200918145614_add_groups_source_to_labs.rb
+++ db/migrate/20200918145614_add_groups_source_to_labs.rb
@@ -1,7 +1,9 @@
 class AddGroupsSourceToLabs < ActiveRecord::Migration[5.2]
   def change
     #Create groups_source column in labs
-    add_reference :labs, :groups_source, index: true, after: :source_id
+    unless column_exists? :labs, :group_source_id
+      add_reference :labs, :groups_source, index: true, after: :source_id
+    end
 
     #for each lab with a status of 'released' or 'retrieved', get groups_source_id for the source attached to the current lab and push that into the groups_source column
     @labs = Lab.where(status: "released").or(Lab.where(status: "retrieved"))
