e0929d3ffe7f7eead9a4cfbcc1273893ab78a02c
Andrew Cates <amcates@gmail.com>
2021-06-15 13:33:37 -0400

    updating mimemagic and testing deploy

--- config/deploy/testing.rb
+++ config/deploy/testing.rb
@@ -1,9 +1,10 @@
-server 'bmic-sparc-dev.obis.musc.edu', user: 'capistrano', roles: %w{app db web}
+server 'bmic-sparc-d.mdc.musc.edu', user: 'capistrano', roles: %w{app db web}
+set :repo_url, "git@living_biobank:living-biobank/living-biobank.git"
 
-set :deploy_to, '/var/www/rails/sparc-biobank'
+set :deploy_to, '/var/www/rails/living-biobank'
 set :branch, 'testing'
 set :rails_env, 'testing'
-set :rvm_ruby_version, '2.5.5@sparc-biobank --create'
+set :rvm_ruby_version, '2.5.5@living-biobank --create'
 set :passenger_restart_with_touch, true
 
 set :default_env, {
