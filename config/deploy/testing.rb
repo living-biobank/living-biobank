420a3d3b679200ef5f1018e16e79141c8ae89e19
Andrew Cates <amcates@gmail.com>
2021-06-16 10:21:41 -0400

    updating deploy script

--- config/deploy/testing.rb
+++ config/deploy/testing.rb
@@ -8,6 +8,6 @@ set :rvm_ruby_version, '2.5.5@living-biobank --create'
 set :passenger_restart_with_touch, true
 
 set :default_env, {
-  'LD_LIBRARY_PATH' => '/usr/lib/oracle/18.3/client64/lib',
-  'CXX' => '/opt/rh/devtoolset-2/root/usr/bin/gcc'
+  'LD_LIBRARY_PATH' => '/usr/lib/oracle/19.9/client64/lib',
+  'CXX' => '/opt/rh/devtoolset-3/root/usr/bin/gcc'
 }
