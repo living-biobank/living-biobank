ef1502eb31594dd6b984731c1ae7bbc7632731b0
Ito <ito.eta1@gmail.com>
2021-09-10 15:25:13 -0400

    minor changes related to endless loading of query dropdowns

--- app/controllers/i2b2_queries_controller.rb
+++ app/controllers/i2b2_queries_controller.rb
@@ -2,17 +2,18 @@ class I2b2QueriesController < ApplicationController
   def index
     respond_to :js
 
+    find_requester
+
     @musc_queries = 
       if params[:protocol_id].present?
         protocol = SPARC::Protocol.find(params[:protocol_id])
         I2b2::Query.where(user_id: protocol.study_users.pluck(:ldap_uid) + (current_user.data_honest_broker? || current_user.admin? ? [current_user.net_id] : []))
       else
-        requester = User.find(params[:user_id])
-        I2b2::Query.where(user_id: requester.net_id)
+        I2b2::Query.where(user_id: @requester.net_id)
       end.order(create_date: :desc)
 
     @shrine_queries = 
-      Shrine::Query.where(username: requester.net_id.split('@').first)
+      Shrine::Query.where(username: @requester.net_id.split('@').first)
   end
 
   def show
@@ -26,7 +27,7 @@ class I2b2QueriesController < ApplicationController
   end
 
   private
-    def requester
-      requester = User.find(params[:user_id])
+    def find_requester
+      @requester = User.find(params[:user_id])
     end
 end
