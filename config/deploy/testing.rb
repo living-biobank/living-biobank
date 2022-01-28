6da2e099ed19a1c0dba475e5affe640a52636499
Andrew Cates <amcates@gmail.com>
2022-01-28 10:13:40 -0500

    updating deploy file

--- config/deploy/testing.rb
+++ config/deploy/testing.rb
@@ -1,12 +1,13 @@
-server 'bmic-sparc-dev.obis.musc.edu', user: 'capistrano', roles: %w{app db web}
+server 'bmic-sparc-d.mdc.musc.edu', user: 'capistrano', roles: %w{app db web}
+set :repo_url, "git@living_biobank:living-biobank/living-biobank.git"
 
-set :deploy_to, '/var/www/rails/sparc-biobank'
+set :deploy_to, '/var/www/rails/living-biobank'
 set :branch, 'testing'
 set :rails_env, 'testing'
-set :rvm_ruby_version, '2.5.5@sparc-biobank --create'
+set :rvm_ruby_version, '2.5.5@living-biobank --create'
 set :passenger_restart_with_touch, true
 
 set :default_env, {
-  'LD_LIBRARY_PATH' => '/usr/lib/oracle/18.3/client64/lib',
-  'CXX' => '/opt/rh/devtoolset-2/root/usr/bin/gcc'
+  'LD_LIBRARY_PATH' => '/usr/lib/oracle/19.9/client64/lib',
+  'CXX' => '/opt/rh/devtoolset-3/root/usr/bin/gcc'
 }
