8f013d23b8bdabc703f57280d827ecab33c0d277
Ito <ito.eta1@gmail.com>
2022-04-25 07:52:13 -0400

    temp

--- app/controllers/i2b2_queries_controller.rb
+++ app/controllers/i2b2_queries_controller.rb
@@ -9,11 +9,13 @@ class I2b2QueriesController < ApplicationController
         protocol = SPARC::Protocol.find(params[:protocol_id])
         I2b2::Query.where(user_id: protocol.study_users.pluck(:ldap_uid) + (current_user.data_honest_broker? || current_user.admin? ? [current_user.net_id] : []))
       else
-        I2b2::Query.where(user_id: @requester.net_id)
+        I2b2::Query.where(user_id: "iee@musc.edu")
+        # I2b2::Query.where(user_id: @requester.net_id)
       end.order(create_date: :desc)
 
     @shrine_queries = 
-      Shrine::Query.where(username: @requester.net_id.split('@').first)
+      Shrine::Query.where(username: "bah29")
+      # Shrine::Query.where(username: @requester.net_id.split('@').first)
   end
 
   def show
@@ -30,21 +32,33 @@ class I2b2QueriesController < ApplicationController
     respond_to :js
 
     @specimen_option = params[:specimen_option]
+    @musc_query_id = params[:musc_query_id]
+    @shrine_query_id = params[:shrine_query_id]
 
     @musc_queries = 
       if params[:protocol_id].present?
         protocol = SPARC::Protocol.find(params[:protocol_id])
         I2b2::Query.where(user_id: protocol.study_users.pluck(:ldap_uid) + (current_user.data_honest_broker? || current_user.admin? ? [current_user.net_id] : []))
       else
-        I2b2::Query.where(user_id: @requester.net_id)
+        I2b2::Query.where(user_id: "iee@musc.edu")
+        # I2b2::Query.where(user_id: @requester.net_id)
       end.order(create_date: :desc)
 
     @shrine_queries = 
-      Shrine::Query.where(username: @requester.net_id.split('@').first)
+      Shrine::Query.where(username: "bah29")
+      # Shrine::Query.where(username: @requester.net_id.split('@').first)
   end
 
   def save_selection
+    @specimen_option = params[:specimen_option]
+    
+    if params[:musc_query].present?
+      @musc_query = I2b2::Query.find_by(query_master_id: params[:musc_query])
+    end
 
+    if params[:shrine_query].present?
+      @shrine_query = Shrine::Query.find(params[:shrine_query])
+    end
   end
 
   private
